64996dbdd5ebfaa1303f9713eacadc136d9bf033
Eric <ericjang310@gmail.com>
2022-06-15 20:38:26 -0700

    frontend boilerplate

---
+++ src/frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        PROJECT 2
+      </header>
+    </div>
+  );
+}
+
+export default App;
